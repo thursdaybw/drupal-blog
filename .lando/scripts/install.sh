a92ff7a2a29de2d303eb6cd694ab86443d8fab71
Bevan Wishart <bevan.wishart@gmail.com>
2023-09-06 16:20:39 +1000

    Fix conflicts with other drupalmatic-containeriser

--- .lando/scripts/install.sh
+++ .lando/scripts/install.sh
@@ -1,2 +1,2 @@
-../vendor/bin/drush site:install --existing-config -y --site-name="Thursday Blog" minimal
-../vendor/bin/drush uli
+./vendor/bin/drush site:install --existing-config -y --site-name="Thursday Blog" minimal
+./vendor/bin/drush uli

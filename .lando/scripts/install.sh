4da99dc80c70551043d2efe013a932f089379681
Bevan Wishart <bevan.wishart@gmail.com>
2023-11-14 11:43:30 +1100

    Add a wait for the database on install

--- .lando/scripts/install.sh
+++ .lando/scripts/install.sh
@@ -1,2 +1,8 @@
+# Wait for the database to be ready
+until mysqladmin ping -hdatabase -P3306 --silent; do
+  echo "Waiting for database to be ready..."
+  sleep 4 
+done
+echo "Database is ready."
 ./vendor/bin/drush site:install --existing-config -y --site-name="Thursday Blog" minimal
 ./vendor/bin/drush uli

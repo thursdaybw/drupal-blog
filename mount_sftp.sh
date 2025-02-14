b83bc50dfbc945a66306554b11330bdc1ccb9d5c
Bevan Wishart <bevan.wishart@gmail.com>
2025-02-14 21:46:17 +1100

    Fix SSHFS mount path in Docker container
    - Corrected remote path from `/data1/home/receipts` to `/data1/home/20187/receipts`
    - Ensured `sshfs` mounts correctly on container startup
    - Updated `mount_sftp.sh` with the correct remote directory
    
    This fixes the "No such file or directory" error when mounting receipts storage.

--- mount_sftp.sh
+++ mount_sftp.sh
@@ -7,7 +7,7 @@ mkdir -p /var/www/html/sites/default/files/receipts
 # Check if it's already mounted
 if ! mountpoint -q /var/www/html/sites/default/files/receipts; then
     sshfs -o IdentityFile=/root/.ssh/id_rsa_sftp,StrictHostKeyChecking=no,UserKnownHostsFile=/dev/null,allow_other \
-          20187@hk-s020.rsync.net:/data1/home/receipts \
+          20187@hk-s020.rsync.net:/data1/home/20187/receipts \
           /var/www/html/sites/default/files/receipts
 fi
 

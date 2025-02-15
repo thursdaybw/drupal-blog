614ce38fd7627a65e14b3a232be5482399479006
Bevan Wishart <bevan.wishart@gmail.com>
2025-02-15 18:10:56 +1100

    commented lines ftw

--- mount_sftp.sh
+++ mount_sftp.sh
@@ -12,16 +12,16 @@ chown www-data:www-data "$SSHFS_MOUNT_POINT"
 # Ensure the bind mount point exists
 mkdir -p "$BIND_MOUNT_POINT"
 
-if ! mountpoint -q "$SSHFS_MOUNT_POINT"; then
+#if ! mountpoint -q "$SSHFS_MOUNT_POINT"; then
 #    echo "Mounting SSHFS at $SSHFS_MOUNT_POINT..."
 #    sshfs -o IdentityFile="$SSH_KEY",StrictHostKeyChecking=no,UserKnownHostsFile=/dev/null,allow_other,uid=33,gid=33,reconnect,sshfs_sync \
 #          20187@hk-s020.rsync.net:/data1/home/20187/receipts "$SSHFS_MOUNT_POINT"
-fi
+#fi
 
-if ! mountpoint -q "$BIND_MOUNT_POINT"; then
+#if ! mountpoint -q "$BIND_MOUNT_POINT"; then
 #    echo "Binding $SSHFS_MOUNT_POINT to $BIND_MOUNT_POINT..."
 #    mount --bind "$SSHFS_MOUNT_POINT" "$BIND_MOUNT_POINT"
-fi
+#fi
 
 exec apache2-foreground
 

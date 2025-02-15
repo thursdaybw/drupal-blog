1689669f614caa5ff42ddc3862089173aca1e00b
Bevan Wishart <bevan.wishart@gmail.com>
2025-02-15 18:02:12 +1100

    Uncomment the SSHFS line and test deploy

--- mount_sftp.sh
+++ mount_sftp.sh
@@ -12,11 +12,11 @@ chown www-data:www-data "$SSHFS_MOUNT_POINT"
 # Ensure the bind mount point exists
 mkdir -p "$BIND_MOUNT_POINT"
 
-#if ! mountpoint -q "$SSHFS_MOUNT_POINT"; then
-#    echo "Mounting SSHFS at $SSHFS_MOUNT_POINT..."
-#    sshfs -o IdentityFile="$SSH_KEY",StrictHostKeyChecking=no,UserKnownHostsFile=/dev/null,allow_other,uid=33,gid=33,reconnect,sshfs_sync \
-#          20187@hk-s020.rsync.net:/data1/home/20187/receipts "$SSHFS_MOUNT_POINT"
-#fi
+if ! mountpoint -q "$SSHFS_MOUNT_POINT"; then
+    echo "Mounting SSHFS at $SSHFS_MOUNT_POINT..."
+    sshfs -o IdentityFile="$SSH_KEY",StrictHostKeyChecking=no,UserKnownHostsFile=/dev/null,allow_other,uid=33,gid=33,reconnect,sshfs_sync \
+          20187@hk-s020.rsync.net:/data1/home/20187/receipts "$SSHFS_MOUNT_POINT"
+fi
 
 #if ! mountpoint -q "$BIND_MOUNT_POINT"; then
 #    echo "Binding $SSHFS_MOUNT_POINT to $BIND_MOUNT_POINT..."

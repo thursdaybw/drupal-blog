3d94013a9c43a079353686fd720665b0f835c98f
Bevan Wishart <bevan.wishart@gmail.com>
2025-07-26 21:29:29 +1000

    Remove debugging code

--- html/modules/custom/bevansbench_test/tests/src/ExistingSiteJavascript/GenerateCaptionsMobileTest.php
+++ html/modules/custom/bevansbench_test/tests/src/ExistingSiteJavascript/GenerateCaptionsMobileTest.php
@@ -5,13 +5,6 @@ use thursdaybw\DttMultiDeviceTestBase\MobileTestBase;
 
 class GenerateCaptionsMobileTest extends MobileTestBase {
   public function testLoginLinkVisible() {
-    file_put_contents(
-      '/tmp/dtt-env.log',
-      'DTT_MINK_DRIVER_ARGS=' . (getenv('DTT_MINK_DRIVER_ARGS') ?: '<unset>') . "\n" .
-      'MINK_DRIVER_ARGS_WEBDRIVER=' . (getenv('MINK_DRIVER_ARGS_WEBDRIVER') ?: '<unset>') . "\n\n",
-      FILE_APPEND
-    );
-
     $this->visit('/');
     $this->assertSession()->elementExists('css', 'nav#block-vani-account-menu a');
   }
